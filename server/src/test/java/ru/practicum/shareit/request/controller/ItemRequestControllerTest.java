83d12a6522723bfb1b033e6e98f5c33bc5cf0cde
Irina Boyko <t.i.sergeevna@yandex.ru>
2025-10-05 17:11:01 +0300

    Fixed field tests

--- server/src/test/java/ru/practicum/shareit/request/controller/ItemRequestControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/request/controller/ItemRequestControllerTest.java
@@ -33,7 +33,7 @@ public class ItemRequestControllerTest {
 
     @Mock
     private ItemRequestService itemRequestService;
-    
+
     @InjectMocks
     private ItemRequestController itemRequestController;
 
